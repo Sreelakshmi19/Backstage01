94d5484e5aca2d0eba014636798809c294989624
Sreelakshmi <indusree037@gmail.com>
2025-07-01 18:41:00 +0530

    add

---
+++ plugins/my-plugin/dev/index.tsx
@@ -0,0 +1,11 @@
+import { createDevApp } from '@backstage/dev-utils';
+import { myPluginPlugin, MyPluginPage } from '../src/plugin';
+
+createDevApp()
+  .registerPlugin(myPluginPlugin)
+  .addPage({
+    element: <MyPluginPage />,
+    title: 'Root Page',
+    path: '/my-plugin',
+  })
+  .render();
